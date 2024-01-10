d8b27a799c180d6a8a0c24f9301400853f02634c
Om Jha <omjha23@gmail.com>
2024-01-10 17:57:52 -0500

    Updated Sprites

---
+++ scenes/BattleManager.gd
@@ -0,0 +1,19 @@
+extends Node
+
+var player_index = 0
+var opp_index = 0
+
+@onready var player_sprite = $PlayerSprite
+@onready var opp_sprite = $EnemySprite
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func load_player_sprite():
+	pass
--- scenes/debug_tool_scene.gd
+++ scenes/debug_tool_scene.gd
@@ -37,3 +37,82 @@ func _on_button_2_pressed():
 	pd.data = save
 	pd.save(1)
 	label.text = "saved! party size: " + str(save.party.size())
+
+
+func _on_button_3_pressed():
+	var paths = ["res://sprites/pokemon/animated", "res://sprites/pokemon/animated/female", "res://sprites/pokemon/animated/shiny", "res://sprites/pokemon/animated/shiny/female", "res://sprites/pokemon/animated/back", "res://sprites/pokemon/animated/back/female", "res://sprites/pokemon/animated/back/shiny", "res://sprites/pokemon/animated/back/shiny/female"]
+	for path in paths:
+		set_sprites(path)
+	
+func set_sprites(path):
+	var dir = DirAccess.open(path)
+	var frames = FileAccess.open(path + "/frames.json", FileAccess.READ)
+	frames = frames.get_as_text()
+	frames = JSON.parse_string(frames)
+	
+	var widths = FileAccess.open(path + "/widths.json", FileAccess.READ)
+	widths = widths.get_as_text()
+	widths = JSON.parse_string(widths)
+	
+	var heights = FileAccess.open(path + "/heights.json", FileAccess.READ)
+	heights = heights.get_as_text()
+	heights = JSON.parse_string(heights)
+	
+	if dir:
+		for filename in dir.get_files():
+			if filename.ends_with(".png"):
+				var image = Image.load_from_file(path + "/" + filename)
+				var tex = ImageTexture.create_from_image(image)
+				var w = widths[filename.trim_suffix(".png")]
+				var h = heights[filename.trim_suffix(".png")]
+				var s = SpriteFrames.new()
+				s.add_animation("default")
+				s.set_animation_loop("default", true)
+				s.set_animation_speed("default", 15.0)
+				var num_frames = frames[filename.trim_suffix(".png")]
+				var x = 0
+				var y = 0
+				label.text = filename
+				for frame in range(num_frames):
+					label.text += "."
+					var atx = AtlasTexture.new()
+					atx.atlas = tex
+					atx.region = Rect2(x*w,y*h,w,h)
+					s.add_frame("default", atx)
+					x += 1
+					if x == 10:
+						x = 0
+						y += 1
+				var res = ResourceSaver.save(s, path + "/" + filename.trim_suffix(".png") + ".res")
+				assert(res == OK)
+
+
+func _on_button_4_pressed():
+	var path = "res://Pokemon"
+	var dir = DirAccess.open(path)
+	
+	if dir:
+		for filename in dir.get_files():
+			var p: Pokemon = ResourceLoader.load(path + "/" + filename, "Pokemon")
+			for entry in p.sprites:
+				p.sprites[entry] = p.sprites[entry].replace("res://sprites/pokemon/versions/generation-v/black-white/animated/", "res://sprites/pokemon/animated/")
+				p.sprites[entry] = p.sprites[entry].replace(".gif", ".png")
+			var res = ResourceSaver.save(p, path + "/" + filename)
+			assert(res == OK)
+	label.text = "done!"
+	
+
+
+func _on_button_5_pressed():
+	var path = "res://Pokemon"
+	var dir = DirAccess.open(path)
+	
+	if dir:
+		for filename in dir.get_files():
+			var p: Pokemon = ResourceLoader.load(path + "/" + filename, "Pokemon")
+			for entry in p.sprites:
+				if p.sprites[entry].contains("animated"):
+					p.sprites[entry] = p.sprites[entry].replace(".png", ".res")
+			var res = ResourceSaver.save(p, path + "/" + filename)
+			assert(res == OK)
+	label.text = "done!"
